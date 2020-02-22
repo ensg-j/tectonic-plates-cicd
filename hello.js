4c54738d0566d2253082abec813459076222ba53
jgrsmaire <julie.grosmaire@ensg.eu>
2020-02-22 18:05:19 +0100

    modifs

--- hello.js
+++ hello.js
@@ -1,6 +1,40 @@
-var http = require('http');
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello Travis!\n'); // build should pass now!
-}).listen(1337, '127.0.0.1');
-console.log('Server running at http://127.0.0.1:1337/');
+var express = require('express');
+var formidable = require('formidable');
+var fs = require('fs');
+
+var app = express();
+
+app.use(express.static(__dirname + '/ressources'));
+
+
+app.get('/', function (req, res){
+    res.sendFile(__dirname + '/ressources/index_form.html');
+});
+
+app.post('/', function (req, res){
+  var form = new formidable.IncomingForm();
+
+  form.parse(req, function(err, fields, files) {
+    //res.writeHead(200, {'content-type': 'text/html'});
+
+    fs.readFile(files.upload.path, function (err, data) {
+      fs.writeFile('ressources/currentfile.geojson',data,function (err) {
+        if (err) throw err;
+        console.log('File is created successfully.');
+      });
+      
+      
+    });
+  });
+
+  res.redirect("/currentglobe");
+});
+
+
+
+app.get('/currentglobe', function (req, res){
+  res.sendFile(__dirname + '/ressources/index_globe.html');
+});
+
+app.listen(3000);
+
